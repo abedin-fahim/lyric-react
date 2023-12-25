52431b7ab3ae740c181e08bac676f0153233eb11
Joynal Abedin <cfcfahim26@gmail.com>
2023-12-25 12:28:52 +0600

    small update

--- src/components/tracks/Tracks.jsx
+++ src/components/tracks/Tracks.jsx
@@ -1,13 +1,17 @@
 import { useLoaderData } from 'react-router-dom';
 import { useGlobalContext } from '../../store/context';
+import { useEffect } from 'react';
 
 const Tracks = () => {
   const tracks = useLoaderData();
   const { heading, trackList, trackListHandler } = useGlobalContext();
-  trackListHandler(tracks);
 
-  console.log(tracks);
+  // console.log(tracks);
   // console.log(trackList);
+
+  useEffect(() => {
+    trackListHandler(tracks);
+  }, [tracks, trackListHandler]);
 
   return (
     <>
