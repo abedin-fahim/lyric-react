695d8de6ea24b2f8f90acb50bcbb5d4ba422e2fc
Joynal Abedin <cfcfahim26@gmail.com>
2024-03-14 19:57:10 +0600

    Hero section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,30 @@
 import Styles from './Hero.module.css';
+import { LuSearchCheck } from 'react-icons/lu';
 
 const Hero = () => {
   return (
     <div className='grid grid-cols-3 gap-4'>
       <div
-        className={`${Styles['hero-container']} col-span-2 p-5 rounded-xl bg-[#609EAF] flex align-middle justify-center flex-col gap-y-6`}
+        className={`${Styles['hero-container']} col-span-2 p-5 rounded-3xl bg-[#649faf] flex align-middle justify-center flex-col gap-y-6`}
       >
-        <span>Curated playlist</span>
-        <div className='flex flex-col'>
-          <h1>R & B Hits</h1>
-          <p>
-            All mine, Lie again, Petty call me everyday, Out of time, No love,
-            Bad habit, and so much more
+        <div className='flex flex-col w-3/5'>
+          <h1 className='mb-3'>Search for any song lyrics!</h1>
+          <p className='text-gray-200'>
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table.
           </p>
+        </div>
+        <div className='w-3/5'>
+          <label
+            className={`input bg-transparent border-white input-bordered border-2 h-auto py-4 flex items-center gap-2 rounded-3xl`}
+          >
+            <input
+              type='text'
+              className={`grow bg-transparent ${Styles['hero-input']}`}
+              placeholder='Type your favorite song title, artist or lyrics'
+            />
+            <LuSearchCheck className='w-5 h-5 text-white' />
+          </label>
         </div>
       </div>
       <div></div>
